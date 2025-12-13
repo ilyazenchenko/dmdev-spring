caebf3483ef9175454c80203b2e24f2d9b7b72c1
Ilya Zenchenko <ilya.zenchenko@icloud.com>
2025-12-13 15:16:19 +0300

    раздел 1 – введение

---
+++ src/main/java/ru/zenchenko/dmdevspring/DmdevSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.zenchenko.dmdevspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmdevSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmdevSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zenchenko/dmdevspring/DmdevSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zenchenko.dmdevspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmdevSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
